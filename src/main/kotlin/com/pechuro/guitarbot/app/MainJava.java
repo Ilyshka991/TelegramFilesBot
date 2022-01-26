9d75b169f03d2482aad81406b751e61cd67c8bab
Ilya Pechuro <ilyapechuro991@gmail.com>
2022-01-26 22:51:44 +0300

    Test

---
+++ src/main/kotlin/com/pechuro/guitarbot/app/MainJava.java
@@ -0,0 +1,12 @@
+package com.pechuro.guitarbot.app;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainJava {
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAA1");
+        Logger.getLogger("AAAAAAA").log(Level.INFO,"DDDDDDDDDD");
+    }
+}
