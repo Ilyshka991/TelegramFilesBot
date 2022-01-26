1e0c1e99a8596672aec5e884810f95ba295d2724
Ilya Pechuro <ilyapechuro991@gmail.com>
2022-01-27 00:40:10 +0300

    Test

--- src/main/java/com/pechuro/guitarbot/app/Configuration.kt
+++ src/main/java/com/pechuro/guitarbot/app/Configuration.kt
@@ -6,17 +6,17 @@ import com.pechuro.BuildConfig
 object Configuration {
 
     object App {
-        const val APPLICATION_NAME: String = BuildConfig.APP_NAME
+        val APPLICATION_NAME: String = System.getenv("application.name") ?: BuildConfig.APP_NAME
         const val MAX_FILES_PER_PAGE = 6
     }
 
     object Google {
-        const val API_KEY: String = BuildConfig.GOOGLE_API_KEY_JSON
-        const val ROOT_FILE_PATH: String = BuildConfig.GOOGLE_ROOT_FILE_PATH
+        val API_KEY: String = System.getenv("google.api_key_json") ?: BuildConfig.GOOGLE_API_KEY_JSON
+        val ROOT_FILE_PATH: String = System.getenv("google.root_file_path") ?: BuildConfig.GOOGLE_ROOT_FILE_PATH
     }
 
     object Telegram {
-        const val TOKEN: String = BuildConfig.TELEGRAM_BOT_TOKEN
+        val TOKEN: String = System.getenv("telegram.bot_token") ?: BuildConfig.TELEGRAM_BOT_TOKEN
         const val TIMEOUT_SEC = 30
         val LOG_LEVEL = LogLevel.Network.Body
     }
